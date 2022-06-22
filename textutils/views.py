01bba926c6ba59dd62e89022fe9f5a54c987f743
Samrat <samratddypppis@gmail.com>
2022-06-22 15:23:45 +0530

    Made the homepage

--- textutils/views.py
+++ textutils/views.py
@@ -1,9 +1,24 @@
 # I have created this file - Samrat
 
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def index(request):
-     return HttpResponse("Hello")
+     return render(request, 'index.html')
 
-def about(request):
-     return HttpResponse("About")
+def removepunc(request):
+     djtext = request.GET.get('text', 'default')
+     print(djtext)
+     return HttpResponse("remove punc")
+
+def capfirst(request):
+     return HttpResponse("Capitalize first")
+
+def newlineremove(request):
+     return HttpResponse("New line remove")
+
+def spaceremove(request):
+     return HttpResponse("Space remove")
+
+def charcount(request):
+     return HttpResponse("charcount")
